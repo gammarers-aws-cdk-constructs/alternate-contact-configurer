2776dbf64208ab1bda0275a41c71ed0a4688bf40
yicr <yicr@users.noreply.github.com>
2026-05-17 05:20:46 +0000

    chore(ci): setting release to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -15,7 +15,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   packageManager: javascript.NodePackageManager.YARN_CLASSIC,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers-aws-cdk-constructs/alternate-contact-configurer.git',
-  releaseToNpm: false,
+  releaseToNpm: true,
   npmTrustedPublishing: false,
   npmAccess: javascript.NpmAccess.PUBLIC,
   mergify: true,
